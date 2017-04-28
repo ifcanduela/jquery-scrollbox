038548d6031739ce0ff7b6d91c467fb2810e2fb2
Igor F. Canduela <ifcanduela@gmail.com>
2017-04-28 12:03:44 +0200

    Added touch events and default CSS

--- README.md
+++ README.md
@@ -2,16 +2,23 @@
 
 A very simple jQuery plugin to add vertical scrollbars to block-level elements.
 
-Requires [jQuery](https://jquery.com/) and the 
+Requires [jQuery](https://jquery.com/) and the
 [Mousewheel plugin](https://github.com/jquery/jquery-mousewheel).
 
 ## Usage
 
-Put a class on the container you want to make scrollable. It must have a 
+Add the script to your HTML:
+
+```html
+<script src="/js/vendor/jquery-scrollbox.js"></script>
+```
+
+Put a class on the container you want to make scrollable. It must have a
 definite height. Then select it with jQuery and call `scrollbox()` on it.
 
 ```js
 $('.scrollable').scrollbox();
 ```
 
-Include the styles from `jquery-scrollable.css` in your stylesheet.
+Finally, include the styles from `jquery-scrollable.css` in your stylesheet (and modify
+to taste).
--- jquery-scrollbox.js
+++ jquery-scrollbox.js
@@ -14,9 +14,13 @@
  */
 (function ($) {
     function resizeKnob($box, $content, $gutter, $knob) {
+        // the visible height of the scrollbox
         let gutter_height = $gutter.height();
+        // the full height of the container
         let content_height = $content.height();
+        // amount of the container that is currently visible
         let knob_height_percent = gutter_height / content_height;
+        // actual size of the knob in pixels
         let knob_height = Math.round(gutter_height * knob_height_percent);
 
         $knob.height(knob_height);
@@ -26,43 +30,66 @@
 
     $.fn.scrollBox = function () {
         return this.each(function () {
+            // the scrollable container
             let $box = $(this);
-
+            // the content to scroll
             let $content = $box.children().first();
-            $content.css('position', 'absolute');
-
+            // a scrolling position marker
             let $knob = $('<div class="js-knob"></div>');
-
+            // the container for the scrollbar
             let $gutter = $('<div class="js-gutter"></div>');
-            $gutter.append($knob);
 
+            // for touch events
+            let _lastTouchOffset = null;
+
+            $content.css('position', 'absolute');
+            $gutter.append($knob);
             $box.append($gutter);
 
+            // resize the knob when it's made visible
             $box.on('mouseenter', function () {
                 resizeKnob($box, $content, $gutter, $knob);
             });
 
-            $box.on('mousewheel', function (event) {
+            // event handlers for mousewheel (via juery plugin) and touch events
+            $box.on('mousewheel touchmove', function (event) {
+                // mousewheel event data
+                let deltaY = event.deltaY;
+                let deltaFactor = event.deltaFactor;
+
+                // switch delta values in case of touch event
+                if (event.type === 'touchmove') {
+                    let _currentTouchOffset = event.touches[0].screenY;
+
+                    deltaY = 1;
+                    deltaFactor = _currentTouchOffset - _lastTouchOffset;
+                    _lastTouchOffset = _currentTouchOffset;
+                }
+
+                // current vertical position of the scrollbox
                 let container_top = parseInt($content.css('top'));
 
+                // full height of the content
                 let content_height = $content.height();
+                // visible height of the container
                 let scrollbox_height = $box.height();
 
-                let height = scrollbox_height - content_height;
-                let offset = event.deltaY * event.deltaFactor;
+                // calculate new position
+                let offset = deltaY * deltaFactor;
                 let calculated_top = container_top + offset;
+                // ensure the scrollbox does not go out of bounds
+                let height = scrollbox_height - content_height;
                 let clamped_top = Math.min(0, Math.max(height, calculated_top));
 
                 $content.css('top', clamped_top);
 
+                // reposition the scrolling marker
                 let knob_height = $knob.height();
                 let gutter_height = $gutter.height();
-
                 let scroll_percent = Math.max(0, Math.min(1, calculated_top / height));
                 let knob_top = (gutter_height - knob_height) * scroll_percent;
 
                 $knob.css('top', knob_top);
-
 
                 return false;
             });
